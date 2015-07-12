ace68ae6bb9f75d804f5f184c72f65dd070462fb
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-07-12 12:28:42 +0200

    working on index page with integration of categories and associated images

--- main.go
+++ main.go
@@ -5,11 +5,30 @@ import (
 	"net/http"
 )
 
-// compile all templates and cache them
-var templates = template.Must(template.ParseGlob("templates/*"))
+type Category struct {
+	Label string
+	Image string
+}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "index", nil)
+type Categories []Category
+
+type IndexContent struct {
+	Categories Categories
+}
+
+// Initialize some global variables
+var categories = Categories{
+	{Label: "Univers jouets enfants", Image: "/static/images/coin_jouets_poupees.png"},
+	{Label: "Mes projets en cours", Image: "/static/images/projets_en_cours.png"},
+	{Label: "Aux pinceaux!", Image: "/static/images/aux_pinceaux.png"},
+	{Label: "Mes plaids tout doux", Image: "/static/images/patc_quilt_plaid.png"},
+	{Label: "Mon coin couture", Image: "static/images/couture1.png"},
+	{Label: "Mes petits objets en carton", Image: "static/images/carton_categ.png"},
+}
+var templates = template.Must(template.ParseGlob("templates/*")) // compile all templates and cache them
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "index", IndexContent{Categories: categories})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -22,7 +41,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	// For now only index
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
 
 	// Start server
 	http.ListenAndServe(":8080", nil)
